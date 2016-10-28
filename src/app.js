112cdbe3d23c7e5ae716ffd7f6287984e711e7ed
Tanawat Tassana <amp.tanawat@gmail.com>
2016-10-29 02:05:51 +0700

    Initialize router with search

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Router, Route, Link, hashHistory } from 'react-router'
 import axios from 'axios'
 import { SearchForm } from './search-form'
 
@@ -13,7 +14,7 @@ const MovieList = (props) => (
     </ul>
 )
 
-class App extends React.Component {
+class Search extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
@@ -39,6 +40,16 @@ class App extends React.Component {
         )
     }
     
+}
+
+class App extends React.Component {
+    render() {
+        return (
+            <Router history={hashHistory}>
+                <Route path="/search" component={Search} />
+            </Router>
+        )
+    }
 }
 
 ReactDOM.render(<App />, document.getElementById('app'))

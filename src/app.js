82548016eea41ae5df6fe7796b7ee69595b94142
Tanawat Tassana <amp.tanawat@gmail.com>
2016-10-29 02:58:47 +0700

    Restructure

--- src/app.js
+++ src/app.js
@@ -2,45 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Router, Route, Link, hashHistory } from 'react-router'
 import axios from 'axios'
-import { SearchForm } from './search-form'
+import { Search } from './search/search' 
 
-const MovieList = (props) => (
-    <ul>
-    {props.movies.map((movie, i) => {
-        return (
-            <li key={i}>{movie.Title}</li>
-        )
-    })}
-    </ul>
-)
 
-class Search extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            movies: []
-        }
-    }
-    onSearch(query) {
-        axios.get(`http://www.omdbapi.com/?s=${query}&plot=short&r=json`)
-            .then(response => {
-                const movies = response.data.Search
-                this.setState({
-                    movies: movies
-                })
-            })
-    }
-    render() {
-        return (
-            <section>
-                <h1>Movie Collection</h1>
-                <SearchForm onSearchSubmit={this.onSearch.bind(this)} />
-                <MovieList movies={this.state.movies} />
-            </section>
-        )
-    }
-    
-}
 
 class App extends React.Component {
     render() {
---
+++ src/search/search.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import axios from 'axios'
+import { SearchForm } from './search-form'
+
+const MovieList = (props) => (
+    <ul>
+    {props.movies.map((movie, i) => {
+        return (
+            <li key={i}>{movie.Title}</li>
+        )
+    })}
+    </ul>
+)
+
+export class Search extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            movies: []
+        }
+    }
+    onSearch(query) {
+        axios.get(`http://www.omdbapi.com/?s=${query}&plot=short&r=json`)
+            .then(response => {
+                const movies = response.data.Search
+                this.setState({
+                    movies: movies
+                })
+            })
+    }
+    render() {
+        return (
+            <section>
+                <h1>Movie Collection</h1>
+                <SearchForm onSearchSubmit={this.onSearch.bind(this)} />
+                <MovieList movies={this.state.movies} />
+            </section>
+        )
+    }
+    
+}

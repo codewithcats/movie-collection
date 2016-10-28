89abfa62bb690a85d80bb23d56340def15b55403
Tanawat Tassana <amp.tanawat@gmail.com>
2016-10-29 04:24:14 +0700

    Nest route

--- src/app.js
+++ src/app.js
@@ -1,19 +1,34 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Router, Route, Link, hashHistory } from 'react-router'
+import { Router, Route, Link, hashHistory, IndexRoute } from 'react-router'
 import axios from 'axios'
 
 import { Home } from './home'
 import { Search } from './search/search'
 
+const Nav = () => (
+    <nav>
+        <a>Home</a>
+        <a>Search</a>
+    </nav>
+)
+
+const Main = props => (
+    <div>
+        <Nav />
+        {props.children}
+    </div>
+)
 
 
 class App extends React.Component {
     render() {
         return (
             <Router history={hashHistory}>
-                <Route path="/" component={Home} />
-                <Route path="/search" component={Search} />
+                <Route path="/" component={Main}>
+                    <IndexRoute component={Home} />
+                    <Route path="search" component={Search} />
+                </Route>
             </Router>
         )
     }

f293c1b6318ecf1b6e163ecd6010bb7f19094aaf
Tanawat Tassana <amp.tanawat@gmail.com>
2016-10-29 10:48:48 +0700

    Nest /search under /

--- src/app.js
+++ src/app.js
@@ -57,7 +57,6 @@ class Search extends React.Component {
     render() {
         return (
             <section>
-                <Nav />
                 <h1>Movie Collection</h1>
                 <SearchForm onSearchSubmit={this.onSearch.bind(this)} />
                 <MovieList movies={this.state.movies} />
@@ -67,16 +66,20 @@ class Search extends React.Component {
     
 }
 
+const App = props => (
+    <section>
+        <Nav />
+        {props.children}
+    </section>
+)
+
 class Main extends React.Component {
     render() {
         return (
             <Router history={hashHistory}>
-                <Route path="/"
-                    component={Home}
-                />
-                <Route path="/search"
-                    component={Search}
-                />
+                <Route path="/" component={App}>
+                    <Route path="search" component={Search}/>
+                </Route>
             </Router>
         )
     }
